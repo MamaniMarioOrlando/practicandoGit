a6060fe44808cc884d2da3e8d55e4b2587615f74
Mario <mario_89_7@hotmail.com>
2020-06-15 11:10:22 -0300

    agrego funcion mostrarVelocidad

--- archivo2.js
+++ archivo2.js
@@ -1,6 +1,14 @@
 let auto={
     nombre:"ford",
     color:"rosa",
-    velocidad:200
+    velocidad:200,
+
+    mostrarVelocida: function()
+    {
+        console.log("la velocidad del auto es: "+this.velocidad);
+    }
+
+
 
 }
+auto.mostrarVelocida();
